e3ec4f77661c6584f0c6ad3fef4640045a13f9e9
Patrick Bahr <patrick.bahr@gmail.com>
2015-04-21 16:46:44 +0200

    code extraction to Haskell

---
+++ Haskell/Extraction.v
@@ -0,0 +1,37 @@
+Add LoadPath "..".
+
+Extract Inductive nat => "Prelude.Int" ["0" "succ"] "(\fO fS n -> if n==0 then fO () else fS (n-1))".
+Extraction Language Haskell.
+
+Require Arith.
+Extraction "Arith.hs" Arith.comp.
+
+Require Exceptions.
+Extraction "Exceptions.hs" Exceptions.comp.
+
+Require ExceptionsTwoCont.
+Extraction "ExceptionsTwoCont.hs" ExceptionsTwoCont.comp.
+
+Require StateGlobal.
+Extraction "StateGlobal.hs" StateGlobal.comp.
+
+Require StateLocal.
+Extraction "StateLocal.hs" StateLocal.comp.
+
+Require StateGlobalSeq.
+Extraction "StateGlobalSeq.hs" StateGlobalSeq.comp.
+
+Require Lambda.
+Extraction "Lambda.hs" Lambda.comp.
+
+Require LambdaExceptions.
+Extraction "LambdaExceptions.hs" LambdaExceptions.comp.
+
+Require LambdaCBName.
+Extraction "LambdaCBName.hs" LambdaCBName.comp.
+
+Require LambdaCBNeed.
+Extraction "LambdaCBNeed.hs" LambdaCBNeed.comp.
+
+Require Loop.
+Extraction "Loop.hs" Loop.comp.
